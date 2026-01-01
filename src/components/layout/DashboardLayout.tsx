bc2b06e6e37a40da230bbb38246ff2b354a07526
Bardia <bardiafarokhsereshti@gmail.com>
2026-01-01 21:14:29 +0330

    glass effect fix

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from './Header';
 
 export const DashboardLayout = () => {
   return (
-    <div className="min-h-screen bg-background font-vazir">
+    <div className="min-h-screen font-vazir bg-transparent">
       <Header />
       <main className="container mx-auto px-4 py-6">
         <Outlet />
